b9189b9e90f451054842fe7bc104adfe676d98d7
admin <hardikothiya@gmail.com>
2021-12-28 16:02:48 +0530

    initiate

---
+++ lib/models/food.dart
@@ -0,0 +1,10 @@
+class Food {
+  Food({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+  });
+  final String imageUrl;
+  final String name;
+  final double price;
+}
