15e2ef7727533f157218b96f1fb229ac84ed936d
admin <hardikothiya@gmail.com>
2021-12-30 18:38:49 +0530

    initiate

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -12,30 +12,146 @@ class _CartScreenState extends State<CartScreen> {
     return (Container(
       padding: EdgeInsets.all(20.0),
       height: 170,
-      child: Row(
-        children: [],
+      child: Expanded(
+        child: Row(
+          children: [
+            Container(
+              width: 150.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(order.food.imageUrl), fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      order.food.name,
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      order.restaurant.name,
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.w600),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(width: 0.8, color: Colors.black54),
+                      ),
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              '-',
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            order.quantity.toString(),
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          SizedBox(width: 20),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              '+',
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: Text(
+                '\$${order.quantity * order.food.price}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach(
+        (Order order) => {totalPrice += order.quantity * order.food.price});
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cart '),
       ),
       body: ListView.separated(
-          itemBuilder: (BuildContext context, int index) {
+        itemCount: currentUser.cart.length + 1,
+        itemBuilder: (BuildContext context, int index) {
+          if (index < currentUser.cart.length) {
             Order order = currentUser.cart[index];
             return _cartBuilder(order);
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider(
-              height: 1,
-              color: Colors.blue,
-            );
-          },
-          itemCount: currentUser.cart.length),
+          }
+          return Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Estimated deliver time :',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold)),
+                  Text(' 25 Min',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold))
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Estimated deliver time :',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold)),
+                  Text(' 25 Min',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold))
+                ],
+              )
+            ],
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(
+            height: 1,
+            color: Colors.blue,
+          );
+        },
+      ),
     );
   }
 }
